118906909be175697129e26a3861f64572a5b401
Maksym Statkevych <maksym@sensogram.com>
2017-10-02 20:24:33 +0300

    add expose ports

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -21,5 +21,5 @@ mvn clean package ;
 cp target/spring-web-project-1.0-SNAPSHOT.war /home/ubuntu/wars/app.war ;
 cp deploy/* /home/ubuntu/wars ;
 cd /home/ubuntu/wars; docker build . -t mytomcat ;
-docker run -d -p 8888:8080 --name mytomContainer mytomcat ;
+docker run -d --name mytomContainer mytomcat ;
 echo " BUILD FINISHED ! ";
